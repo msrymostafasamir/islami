f5cf377cb7d45d4d98c4447277b8d64d916d1ba7
msrymostafasamir <mos.samir1987@gmail.com>
2022-08-01 12:14:22 +0200

    initial project

---
+++ lib/HOme/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+  backgroundColor:Colors.red ,
+  title: Text('Islami'),
+),
+    );
+  }
+}
